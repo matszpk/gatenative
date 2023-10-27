36a14d6e044b6a33b67cf8b61d8118779ed103ce
matszpk <matszpk@interia.pl>
2023-10-27 08:43:45 +0200

    Add next testcase for optimize_negs_to_occurs - XOR subtrees.

--- src/lib.rs
+++ src/lib.rs
@@ -2419,5 +2419,39 @@ mod tests {
             },
             circuit
         );
+
+        // handling XOR subtrees
+        for t in [false, true] {
+            let mut circuit = VBinOpCircuit {
+                input_len: 3,
+                gates: vec![
+                    (vgate_and(0, 1), NegOutput),
+                    (vgate_or(0, 2), NegOutput),
+                    (vgate_and(0, 2), if t { NegOutput } else { NoNegs }),
+                    (vgate_xor(3, 4), NoNegs),
+                    (vgate_xor(5, 3), NoNegs),
+                    (vgate_xor(6, 7), NoNegs),
+                ],
+                outputs: vec![(8, true)],
+            };
+            let xor_map = circuit.xor_subtree_map();
+            let occurs = circuit.occurrences();
+            circuit.optimize_negs_to_occurs(&occurs, xor_map);
+            assert_eq!(
+                VBinOpCircuit {
+                    input_len: 3,
+                    gates: vec![
+                        (vgate_and(0, 1), NoNegs),
+                        (vgate_or(0, 2), NoNegs),
+                        (vgate_and(0, 2), NoNegs),
+                        (vgate_xor(3, 4), NoNegs),
+                        (vgate_xor(5, 3), NoNegs),
+                        (vgate_xor(6, 7), NoNegs),
+                    ],
+                    outputs: vec![(8, t)],
+                },
+                circuit
+            );
+        }
     }
 }
