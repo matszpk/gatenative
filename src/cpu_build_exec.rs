74cfd0bfef4d7dc46e0ea9fd054ccd2e9f1db571
matszpk <matszpk@interia.pl>
2023-11-17 14:11:07 +0100

    Fixed detect_cpu. Add testcases for detection of CPU.

--- src/cpu_build_exec.rs
+++ src/cpu_build_exec.rs
@@ -7,13 +7,11 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 enum DetectCPUError {
-    #[error("Unknown error")]
-    SyntaxError,
     #[error("IO error {0}")]
     IOError(#[from] io::Error),
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum CPUExtension {
     NoExtension,
     IntelMMX,
@@ -23,8 +21,10 @@ enum CPUExtension {
     ARMNEON,
 }
 
-fn detect_cpu(file: impl BufRead) -> Result<CPUExtension, DetectCPUError> {
+fn detect_cpu_from_file(file: impl BufRead) -> Result<CPUExtension, DetectCPUError> {
     let mut have_fp = false;
+    let mut is_armv8 = false;
+    let mut have_flags = false;
     for rl in file.lines() {
         let line = rl?;
         if line.starts_with("flags") || line.starts_with("Features") {
@@ -35,20 +35,83 @@ fn detect_cpu(file: impl BufRead) -> Result<CPUExtension, DetectCPUError> {
             } else if line.find(" sse").is_some() {
                 return Ok(CPUExtension::IntelSSE);
             } else if line.find(" mmx").is_some() {
-                return Ok(CPUExtension::IntelSSE);
+                return Ok(CPUExtension::IntelMMX);
             } else if line.find(" neon").is_some() {
                 return Ok(CPUExtension::ARMNEON);
             } else if line.find(" fp").is_some() {
                 have_fp = true;
             }
+            have_flags = true;
         } else if line.starts_with("CPU architecture: 8") {
-            // NEON is default for ARMv8 with FP.
-            return if have_fp {
-                Ok(CPUExtension::ARMNEON)
+            is_armv8 = true;
+        }
+        if have_flags && is_armv8 {
+            if have_fp {
+                // NEON is default for ARMv8 with FP.
+                return Ok(CPUExtension::ARMNEON);
             } else {
-                Ok(CPUExtension::NoExtension)
-            };
+                return Ok(CPUExtension::NoExtension);
+            }
         }
     }
     Ok(CPUExtension::NoExtension)
+}
+
+fn detect_cpu() -> Result<CPUExtension, DetectCPUError> {
+    detect_cpu_from_file(BufReader::new(File::open("/proc/cpuinfo")?))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_detect_cpu_from_file() {
+        assert_eq!(
+            CPUExtension::NoExtension,
+            detect_cpu_from_file(&mut BufReader::new(&b"flags\t\t: fpu"[..])).unwrap()
+        );
+        assert_eq!(
+            CPUExtension::IntelMMX,
+            detect_cpu_from_file(&mut BufReader::new(&b"flags\t\t: fpu mmx"[..])).unwrap()
+        );
+        assert_eq!(
+            CPUExtension::IntelSSE,
+            detect_cpu_from_file(&mut BufReader::new(&b"flags\t\t: fpu mmx sse sse2"[..])).unwrap()
+        );
+        assert_eq!(
+            CPUExtension::IntelAVX,
+            detect_cpu_from_file(&mut BufReader::new(&b"flags\t\t: fpu mmx sse sse2 avx"[..]))
+                .unwrap()
+        );
+        assert_eq!(
+            CPUExtension::IntelAVX512,
+            detect_cpu_from_file(&mut BufReader::new(
+                &b"flags\t\t: fpu mmx sse sse2 avx avx512f"[..]
+            ))
+            .unwrap()
+        );
+        assert_eq!(
+            CPUExtension::NoExtension,
+            detect_cpu_from_file(&mut BufReader::new(&b"Features\t: fp"[..])).unwrap()
+        );
+        assert_eq!(
+            CPUExtension::ARMNEON,
+            detect_cpu_from_file(&mut BufReader::new(&b"Features\t: fp neon"[..])).unwrap()
+        );
+        assert_eq!(
+            CPUExtension::NoExtension,
+            detect_cpu_from_file(&mut BufReader::new(
+                &b"Features\t: xxx\nCPU architecture: 8\n"[..]
+            ))
+            .unwrap()
+        );
+        assert_eq!(
+            CPUExtension::ARMNEON,
+            detect_cpu_from_file(&mut BufReader::new(
+                &b"Features\t: fp\nCPU architecture: 8\n"[..]
+            ))
+            .unwrap()
+        );
+    }
 }
