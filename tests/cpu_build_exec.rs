7cb574393562743840157b90cbe302e189eb9b63
matszpk <matszpk@interia.pl>
2023-11-19 18:54:45 +0100

    Extend testcase.

--- tests/cpu_build_exec.rs
+++ tests/cpu_build_exec.rs
@@ -5,50 +5,57 @@ use gatesim::*;
 
 #[test]
 fn test_cpu_builder_and_exec() {
-    //let no_opt_neg_config = CPUBuilderConfig{ optimize_negs: false };
-    let mut builder = CPUBuilder::new_with_cpu_ext_and_clang_config(
-        CPUExtension::NoExtension,
-        &CLANG_WRITER_U64_TEST_IMPL,
-        None,
-    );
-    let circuit = Circuit::new(
-        4,
-        [
-            Gate::new_and(0, 2),
-            Gate::new_and(1, 2),
-            Gate::new_and(0, 3),
-            Gate::new_and(1, 3),
-            // add a1*b0 + a0*b1
-            Gate::new_xor(5, 6),
-            Gate::new_and(5, 6),
-            // add c(a1*b0 + a0*b1) + a1*b1
-            Gate::new_xor(7, 9),
-            Gate::new_and(7, 9),
-        ],
-        [(4, false), (8, false), (10, false), (11, false)],
-    )
-    .unwrap();
-    builder.add("mul2x2", circuit, None, None);
-    let mut execs = builder.build().unwrap();
-    let exec = &mut execs[0];
-    const MUL2X2_INPUT: [u32; 8] = [
-        0b1010101010101010,
-        0u32,
-        0b1100110011001100,
-        0u32,
-        0b1111000011110000,
-        0u32,
-        0b1111111100000000,
-        0u32,
-    ];
-    let out = exec.execute(&MUL2X2_INPUT[..]).unwrap();
-    for i in 0..16 {
-        let a = i & 3;
-        let b = i >> 2;
-        let c = ((out[0] >> i) & 1)
-            + (((out[2] >> i) & 1) << 1)
-            + (((out[4] >> i) & 1) << 2)
-            + (((out[6] >> i) & 1) << 3);
-        assert_eq!((a * b) & 15, c, "{}", i);
+    use CPUExtension::*;
+    let no_opt_neg_config = CPUBuilderConfig {
+        optimize_negs: false,
+    };
+    for (cpu_ext, writer_config, builder_config) in [
+        (NoExtension, &CLANG_WRITER_U64_TEST_IMPL, None),
+        (NoExtension, &CLANG_WRITER_U64_TEST_NIMPL, None),
+        (NoExtension, &CLANG_WRITER_U64, None),
+        (NoExtension, &CLANG_WRITER_U64, Some(no_opt_neg_config)),
+    ] {
+        let mut builder =
+            CPUBuilder::new_with_cpu_ext_and_clang_config(cpu_ext, writer_config, builder_config);
+        let circuit = Circuit::new(
+            4,
+            [
+                Gate::new_and(0, 2),
+                Gate::new_and(1, 2),
+                Gate::new_and(0, 3),
+                Gate::new_and(1, 3),
+                // add a1*b0 + a0*b1
+                Gate::new_xor(5, 6),
+                Gate::new_and(5, 6),
+                // add c(a1*b0 + a0*b1) + a1*b1
+                Gate::new_xor(7, 9),
+                Gate::new_and(7, 9),
+            ],
+            [(4, false), (8, false), (10, false), (11, false)],
+        )
+        .unwrap();
+        builder.add("mul2x2", circuit, None, None);
+        let mut execs = builder.build().unwrap();
+        let exec = &mut execs[0];
+        const MUL2X2_INPUT: [u32; 8] = [
+            0b1010101010101010,
+            0u32,
+            0b1100110011001100,
+            0u32,
+            0b1111000011110000,
+            0u32,
+            0b1111111100000000,
+            0u32,
+        ];
+        let out = exec.execute(&MUL2X2_INPUT[..]).unwrap();
+        for i in 0..16 {
+            let a = i & 3;
+            let b = i >> 2;
+            let c = ((out[0] >> i) & 1)
+                + (((out[2] >> i) & 1) << 1)
+                + (((out[4] >> i) & 1) << 2)
+                + (((out[6] >> i) & 1) << 3);
+            assert_eq!((a * b) & 15, c, "{}", i);
+        }
     }
 }
