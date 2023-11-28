32dc1aca6ecb95b18358428e9472d4f0772fa6e2
matszpk <matszpk@interia.pl>
2023-11-28 10:38:23 +0100

    Write preliminary version of decouple_circuit.

--- src/divide.rs
+++ src/divide.rs
@@ -1,6 +1,6 @@
 use gatesim::*;
 
-use std::collections::{BTreeSet, HashSet};
+use std::collections::{BTreeMap, BTreeSet, HashSet};
 use std::fmt::Debug;
 
 pub(crate) struct Placement {
@@ -34,7 +34,7 @@ where
     let gates = circuit.gates();
     let mut cur_nodes = HashSet::<usize>::from_iter(roots.into_iter().copied());
     let mut next_nodes = HashSet::<usize>::new();
-    let mut visited = BTreeSet::<usize>::new();
+    let mut visited = BTreeMap::<usize, usize>::new();
     let mut new_inputs = BTreeSet::<usize>::new();
 
     // use BFS - breadth first search.
@@ -43,7 +43,7 @@ where
         for node in &cur_nodes {
             if *node < input_len {
                 new_inputs.insert(*node);
-            } else if !visited.contains(&node) {
+            } else if !visited.contains_key(&node) {
                 let g = gates[node - input_len];
                 next_nodes.insert(usize::try_from(g.i0).unwrap());
                 next_nodes.insert(usize::try_from(g.i1).unwrap());
@@ -52,17 +52,59 @@ where
         if depth >= min_depth && visited.len() + cur_nodes.len() >= max_gates {
             break;
         }
-        visited.extend(cur_nodes.clone());
+        // insert to visited map
+        for node in &cur_nodes {
+            if *node >= input_len && !visited.contains_key(&node) {
+                visited.insert(*node, visited.len());
+            }
+        }
         std::mem::swap(&mut cur_nodes, &mut next_nodes);
         next_nodes.clear();
         depth += 1;
     }
     // collect new inputs
     new_inputs.extend(cur_nodes);
+    let new_inputs = BTreeMap::from_iter(new_inputs.into_iter().enumerate().map(|(i, x)| (x, i)));
+    let new_input_len = new_inputs.len();
 
     (
-        Circuit::<T>::new(T::try_from(new_inputs.len()).unwrap(), [], []).unwrap(),
-        Vec::<usize>::from_iter(new_inputs),
+        Circuit::<T>::new(
+            T::try_from(new_input_len).unwrap(),
+            visited
+                .iter()
+                .map(|(orig, new)| {
+                    let g = gates[*orig];
+                    let gi0 = usize::try_from(g.i0).unwrap();
+                    let gi1 = usize::try_from(g.i0).unwrap();
+                    Gate {
+                        func: g.func,
+                        i0: T::try_from(if g.i0 >= circuit.input_len() {
+                            if let Some(idx) = visited.get(&gi0) {
+                                idx + new_input_len
+                            } else {
+                                new_inputs[&gi0]
+                            }
+                        } else {
+                            new_inputs[&gi0]
+                        })
+                        .unwrap(),
+                        i1: T::try_from(if g.i1 >= circuit.input_len() {
+                            if let Some(idx) = visited.get(&gi1) {
+                                idx + new_input_len
+                            } else {
+                                new_inputs[&gi1]
+                            }
+                        } else {
+                            new_inputs[&gi1]
+                        })
+                        .unwrap(),
+                    }
+                })
+                .collect::<Vec<_>>(),
+            roots.iter().map(|x| (T::try_from(*x).unwrap(), false)),
+        )
+        .unwrap(),
+        Vec::<usize>::from_iter(new_inputs.keys().copied()),
     )
 }
 
