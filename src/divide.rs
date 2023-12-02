2295c21294d77036872b5aaa373a17b295e41aa3
matszpk <matszpk@interia.pl>
2023-12-02 11:08:38 +0100

    Next commenting.

--- src/divide.rs
+++ src/divide.rs
@@ -35,7 +35,7 @@ pub(crate) struct DivCircuit<T: Clone + Copy>(Vec<DivCircuitEntry<T>>);
 //         only possible if next subcircuit doesn't have shared gates between
 //         current subcircuit except way from next subcircuit is connected.
 //    3.3. remove that ways where are overflows. and go to 1. to create next subcircuit
-//         starting from end of way where is overflow.
+//         starting from end (to circuit output) of way where is overflow.
 // hint: try use one buffer as input and output for one subcircuit.
 // it is possible because code generator always load data first and store data at end of code.
 
