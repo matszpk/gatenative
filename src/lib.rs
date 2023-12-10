446ac47a4caf256bd89628fa526152c19e91f555
matszpk <matszpk@interia.pl>
2023-12-10 16:08:02 +0100

    Simplify add in MapperBuilder.

--- src/lib.rs
+++ src/lib.rs
@@ -326,30 +326,13 @@ pub trait MapperExecutor {
 pub trait MapperBuilder<E: MapperExecutor> {
     type ErrorType;
 
-    fn add<T>(
-        &mut self,
-        name: &str,
-        circuit: Circuit<T>,
-        input_placement: Option<(&[usize], usize)>,
-        output_placement: Option<(&[usize], usize)>,
-        arg_inputs: Option<&[usize]>,
-    ) where
-        T: Clone + Copy + Ord + PartialEq + Eq + Hash,
-        T: Default + TryFrom<usize>,
-        <T as TryFrom<usize>>::Error: Debug,
-        usize: TryFrom<T>,
-        <usize as TryFrom<T>>::Error: Debug;
-
-    fn add_simple<T>(&mut self, name: &str, circuit: Circuit<T>)
+    fn add<T>(&mut self, name: &str, circuit: Circuit<T>)
     where
         T: Clone + Copy + Ord + PartialEq + Eq + Hash,
         T: Default + TryFrom<usize>,
         <T as TryFrom<usize>>::Error: Debug,
         usize: TryFrom<T>,
-        <usize as TryFrom<T>>::Error: Debug,
-    {
-        self.add(name, circuit, None, None, None);
-    }
+        <usize as TryFrom<T>>::Error: Debug;
 
     fn build(self) -> Result<Vec<E>, Self::ErrorType>;
 }
