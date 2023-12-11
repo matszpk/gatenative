a0ba90b0de4f7376ea05c1bc3beca0b965d01f9c
matszpk <matszpk@interia.pl>
2023-12-11 08:23:37 +0100

    Add BasicMapperBuilder.

--- src/mapper.rs
+++ src/mapper.rs
@@ -1,4 +1,3 @@
-use crate::divide::*;
 use crate::*;
 use std::fmt::Debug;
 use std::hash::Hash;
@@ -62,5 +61,58 @@ where
 
     fn new_data_from_vec(&mut self, data: Vec<u32>) -> D {
         self.executor.new_data_from_vec(data)
+    }
+}
+
+pub struct BasicMapperBuilder<'a, DR, DW, D, E, B>
+where
+    DR: DataReader,
+    DW: DataWriter,
+    D: DataHolder<'a, DR, DW>,
+    E: Executor<'a, DR, DW, D>,
+    B: Builder<'a, DR, DW, D, E>,
+{
+    builder: B,
+    d: PhantomData<&'a D>,
+    dr: PhantomData<&'a DR>,
+    dw: PhantomData<&'a DW>,
+    e: PhantomData<&'a E>,
+}
+
+impl<'a, DR, DW, D, E, B> MapperBuilder<'a, DR, DW, D, BasicMapperExecutor<'a, DR, DW, D, E>>
+    for BasicMapperBuilder<'a, DR, DW, D, E, B>
+where
+    DR: DataReader,
+    DW: DataWriter,
+    D: DataHolder<'a, DR, DW>,
+    E: Executor<'a, DR, DW, D>,
+    B: Builder<'a, DR, DW, D, E>,
+{
+    type ErrorType = B::ErrorType;
+
+    fn add<T>(&mut self, name: &str, circuit: Circuit<T>, arg_inputs: &[usize])
+    where
+        T: Clone + Copy + Ord + PartialEq + Eq + Hash,
+        T: Default + TryFrom<usize>,
+        <T as TryFrom<usize>>::Error: Debug,
+        usize: TryFrom<T>,
+        <usize as TryFrom<T>>::Error: Debug,
+    {
+        self.builder.add(name, circuit, None, None, Some(arg_inputs));
+    }
+
+    fn build(self) -> Result<Vec<BasicMapperExecutor<'a, DR, DW, D, E>>, Self::ErrorType> {
+        Ok(vec![])
+    }
+
+    fn word_len(&self) -> u32 {
+        self.builder.word_len()
+    }
+
+    fn is_data_holder_global() -> bool {
+        B::is_data_holder_global()
+    }
+    fn is_data_holder_in_builder() -> bool {
+        B::is_data_holder_in_builder()
     }
 }
