d80885a5fcec510b688c6de1b34c653ef834f0f3
matszpk <matszpk@interia.pl>
2023-12-18 08:19:51 +0100

    First testcases for ParSeqAllDataHolder.

--- tests/parseq_mapper.rs
+++ tests/parseq_mapper.rs
@@ -24,6 +24,50 @@ fn test_parseq_mapper_data_holder() {
             OpenCLBuilder::new(&device, None)
         })
         .collect::<Vec<_>>();
+    let seq_num = seq_builders.len();
 
-    let parseq_builder = ParSeqMapperBuilder::new(par_builder, seq_builders);
+    let mut builder = ParSeqMapperBuilder::new(par_builder, seq_builders);
+    let circuit = Circuit::new(4, [], [(0, false), (1, false), (2, false), (3, false)]).unwrap();
+    builder.add("mul2x2", circuit.clone(), &[0, 1]);
+    let mut execs = builder.build().unwrap();
+    let mut data = execs[0].new_data(96);
+    let selections = std::iter::once(ParSeqSelection::Par)
+        .chain((0..seq_num).map(|i| ParSeqSelection::Seq(i)))
+        .collect::<Vec<_>>();
+    println!("selections: {:?}", selections);
+    data.process_mut(|obj| match obj {
+        ParSeqObject::Par(sel_data) => {
+            let mut wr = sel_data.get_mut();
+            for (i, x) in wr.get_mut().iter_mut().enumerate() {
+                *x = u32::try_from(i * 1100 + 0).unwrap();
+            }
+        }
+        ParSeqObject::Seq((si, sel_data)) => {
+            let mut wr = sel_data.get_mut();
+            for (i, x) in wr.get_mut().iter_mut().enumerate() {
+                *x = u32::try_from(i * 1100 + 1 + si).unwrap();
+            }
+        }
+    });
+    // check
+    data.process(|obj| match obj {
+        ParSeqObject::Par(sel_data) => {
+            let rd = sel_data.get();
+            for (i, x) in rd.get().iter().enumerate() {
+                assert_eq!(*x, u32::try_from(i * 1100 + 0).unwrap(), "{} {}", 0, i);
+            }
+        }
+        ParSeqObject::Seq((si, sel_data)) => {
+            let rd = sel_data.get();
+            for (i, x) in rd.get().iter().enumerate() {
+                assert_eq!(
+                    *x,
+                    u32::try_from(i * 1100 + 1 + si).unwrap(),
+                    "{} {}",
+                    si,
+                    i
+                );
+            }
+        }
+    });
 }
