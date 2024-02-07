c0bcecca36d2e6abbfa7f44918f24e149866f971
matszpk <matszpk@interia.pl>
2024-02-07 16:39:04 +0100

    Revert changes.

--- src/gencode.rs
+++ src/gencode.rs
@@ -3,7 +3,6 @@ use gatesim::*;
 
 use int_enum::IntEnum;
 
-use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
 use std::hash::Hash;
 
@@ -51,11 +50,7 @@ fn single_var_use<T>(
     }
 }
 
-fn gen_var_allocs<T>(
-    circuit: &Circuit<T>,
-    var_usage: &mut [T],
-    single_buffer: bool,
-) -> (Vec<T>, usize)
+fn gen_var_allocs<T>(circuit: &Circuit<T>, var_usage: &mut [T]) -> (Vec<T>, usize)
 where
     T: Clone + Copy + Ord + PartialEq + Eq + Hash,
     T: Default + TryFrom<usize>,
@@ -76,17 +71,9 @@ where
     let mut var_alloc = VarAllocator::<T>::new();
 
     let mut visited = vec![false; gate_num];
-    if single_buffer {
-        for i in 0..input_len {
-            single_var_alloc(&mut var_alloc, &mut alloc_vars, T::try_from(i).unwrap());
-        }
+    for i in 0..input_len {
+        single_var_alloc(&mut var_alloc, &mut alloc_vars, T::try_from(i).unwrap());
     }
-
-    let out_map = if !single_buffer {
-        HashSet::from_iter(circuit.outputs().iter().map(|(o, _)| *o))
-    } else {
-        HashSet::new()
-    };
 
     for (o, _) in circuit.outputs().iter() {
         if *o < input_len_t {
@@ -129,40 +116,20 @@ where
             } else {
                 // allocate and use
                 //println!("Stack: {:?} {:?}", input_len + node_index, gates[node_index]);
-                if !single_buffer {
-                    // allocate circuit inputs now if not allocated
-                    if gates[node_index].i0 < input_len_t {
-                        single_var_alloc(&mut var_alloc, &mut alloc_vars, gates[node_index].i0);
-                    }
-                    if gates[node_index].i1 < input_len_t {
-                        single_var_alloc(&mut var_alloc, &mut alloc_vars, gates[node_index].i1);
-                    }
-                }
                 single_var_use(&mut var_alloc, &alloc_vars, var_usage, gates[node_index].i0);
                 single_var_use(&mut var_alloc, &alloc_vars, var_usage, gates[node_index].i1);
-                let tnode = T::try_from(node_index + input_len).unwrap();
-                single_var_alloc(&mut var_alloc, &mut alloc_vars, tnode);
-                if !single_buffer && out_map.contains(&tnode) {
-                    // use output at this point
-                    single_var_use(&mut var_alloc, &alloc_vars, var_usage, tnode);
-                }
+                single_var_alloc(
+                    &mut var_alloc,
+                    &mut alloc_vars,
+                    T::try_from(node_index + input_len).unwrap(),
+                );
                 stack.pop();
             }
         }
     }
 
-    if single_buffer {
-        for (o, _) in circuit.outputs().iter() {
-            single_var_use(&mut var_alloc, &alloc_vars, var_usage, *o);
-        }
-    } else {
-        // for outputs just
-        for (o, _) in circuit.outputs().iter() {
-            if *o < input_len_t {
-                single_var_alloc(&mut var_alloc, &mut alloc_vars, *o);
-                single_var_use(&mut var_alloc, &alloc_vars, var_usage, *o);
-            }
-        }
+    for (o, _) in circuit.outputs().iter() {
+        single_var_use(&mut var_alloc, &alloc_vars, var_usage, *o);
     }
 
     (
@@ -179,7 +146,6 @@ fn gen_func_code_for_ximpl<FW: FuncWriter, T>(
     circuit: &VCircuit<T>,
     swap_args: &[bool],
     var_allocs: &[T],
-    single_buffer: bool,
 ) where
     T: Clone + Copy + Ord + PartialEq + Eq + Hash,
     T: Default + TryFrom<usize>,
@@ -196,21 +162,6 @@ fn gen_func_code_for_ximpl<FW: FuncWriter, T>(
     let input_len = usize::try_from(input_len_t).unwrap();
     let gate_num = circuit.gates.len();
     let gates = &circuit.gates;
-
-    let out_map = if !single_buffer {
-        let mut out_map = HashMap::<T, Vec<(usize, bool)>>::new();
-        for (i, (o, n)) in circuit.outputs.iter().enumerate() {
-            if let Some(outlist) = out_map.get_mut(o) {
-                outlist.push((i, *n));
-            } else {
-                out_map.insert(*o, vec![(i, *n)]);
-            }
-        }
-        out_map
-    } else {
-        HashMap::new()
-    };
-    let mut used_inputs = vec![false; input_len];
 
     let mut visited = vec![false; gate_num];
     for (o, _) in circuit.outputs.iter() {
@@ -260,18 +211,6 @@ fn gen_func_code_for_ximpl<FW: FuncWriter, T>(
                     });
                 }
             } else {
-                if !single_buffer {
-                    let gi0 = usize::try_from(gates[node_index].i0).unwrap();
-                    let gi1 = usize::try_from(gates[node_index].i1).unwrap();
-                    if gi0 < input_len && !used_inputs[gi0] {
-                        writer.gen_load(usize::try_from(var_allocs[gi0]).unwrap(), gi0);
-                        used_inputs[gi0] = true;
-                    }
-                    if gi1 < input_len && !used_inputs[gi1] {
-                        writer.gen_load(usize::try_from(var_allocs[gi1]).unwrap(), gi1);
-                        used_inputs[gi1] = true;
-                    }
-                }
                 writer.gen_op(
                     match gates[node_index].func {
                         VGateFunc::And => InstrOp::And,
@@ -290,47 +229,17 @@ fn gen_func_code_for_ximpl<FW: FuncWriter, T>(
                     usize::try_from(var_allocs[usize::try_from(gates[node_index].i1).unwrap()])
                         .unwrap(),
                 );
-                if !single_buffer {
-                    let tnode = T::try_from(input_len + node_index).unwrap();
-                    if let Some(outlist) = out_map.get(&tnode) {
-                        for (oi, on) in outlist {
-                            // if output then store
-                            writer.gen_store(
-                                *on,
-                                *oi,
-                                usize::try_from(var_allocs[input_len + node_index]).unwrap(),
-                            );
-                        }
-                    }
-                }
                 stack.pop();
             }
         }
     }
 
-    if single_buffer {
-        for (oi, (o, on)) in circuit.outputs.iter().enumerate() {
-            writer.gen_store(
-                *on,
-                oi,
-                usize::try_from(var_allocs[usize::try_from(*o).unwrap()]).unwrap(),
-            );
-        }
-    } else {
-        for (oi, (o, on)) in circuit.outputs.iter().enumerate() {
-            if *o < input_len_t {
-                let ou = usize::try_from(*o).unwrap();
-                if !used_inputs[ou] {
-                    writer.gen_load(usize::try_from(var_allocs[ou]).unwrap(), ou);
-                    used_inputs[ou] = true;
-                }
-                writer.gen_store(
-                    *on,
-                    oi,
-                    usize::try_from(var_allocs[usize::try_from(*o).unwrap()]).unwrap(),
-                );
-            }
-        }
+    for (oi, (o, on)) in circuit.outputs.iter().enumerate() {
+        writer.gen_store(
+            *on,
+            oi,
+            usize::try_from(var_allocs[usize::try_from(*o).unwrap()]).unwrap(),
+        );
     }
 }
 
@@ -339,7 +248,6 @@ fn gen_func_code_for_binop<FW: FuncWriter, T>(
     circuit: &VBinOpCircuit<T>,
     swap_args: &[bool],
     var_allocs: &[T],
-    single_buffer: bool,
 ) where
     T: Clone + Copy + Ord + PartialEq + Eq + Hash,
     T: Default + TryFrom<usize>,
@@ -356,21 +264,6 @@ fn gen_func_code_for_binop<FW: FuncWriter, T>(
     let input_len = usize::try_from(input_len_t).unwrap();
     let gate_num = circuit.gates.len();
     let gates = &circuit.gates;
-
-    let out_map = if !single_buffer {
-        let mut out_map = HashMap::<T, Vec<(usize, bool)>>::new();
-        for (i, (o, n)) in circuit.outputs.iter().enumerate() {
-            if let Some(outlist) = out_map.get_mut(o) {
-                outlist.push((i, *n));
-            } else {
-                out_map.insert(*o, vec![(i, *n)]);
-            }
-        }
-        out_map
-    } else {
-        HashMap::new()
-    };
-    let mut used_inputs = vec![false; input_len];
 
     let mut visited = vec![false; gate_num];
     for (o, _) in circuit.outputs.iter() {
@@ -420,18 +313,6 @@ fn gen_func_code_for_binop<FW: FuncWriter, T>(
                     });
                 }
             } else {
-                if !single_buffer {
-                    let gi0 = usize::try_from(gates[node_index].0.i0).unwrap();
-                    let gi1 = usize::try_from(gates[node_index].0.i1).unwrap();
-                    if gi0 < input_len && !used_inputs[gi0] {
-                        writer.gen_load(usize::try_from(var_allocs[gi0]).unwrap(), gi0);
-                        used_inputs[gi0] = true;
-                    }
-                    if gi1 < input_len && !used_inputs[gi1] {
-                        writer.gen_load(usize::try_from(var_allocs[gi1]).unwrap(), gi1);
-                        used_inputs[gi1] = true;
-                    }
-                }
                 writer.gen_op(
                     match gates[node_index].0.func {
                         VGateFunc::And => InstrOp::And,
@@ -448,47 +329,17 @@ fn gen_func_code_for_binop<FW: FuncWriter, T>(
                     usize::try_from(var_allocs[usize::try_from(gates[node_index].0.i1).unwrap()])
                         .unwrap(),
                 );
-                if !single_buffer {
-                    let tnode = T::try_from(input_len + node_index).unwrap();
-                    if let Some(outlist) = out_map.get(&tnode) {
-                        for (oi, on) in outlist {
-                            // if output then store
-                            writer.gen_store(
-                                *on,
-                                *oi,
-                                usize::try_from(var_allocs[input_len + node_index]).unwrap(),
-                            );
-                        }
-                    }
-                }
                 stack.pop();
             }
         }
     }
 
-    if single_buffer {
-        for (oi, (o, on)) in circuit.outputs.iter().enumerate() {
-            writer.gen_store(
-                *on,
-                oi,
-                usize::try_from(var_allocs[usize::try_from(*o).unwrap()]).unwrap(),
-            );
-        }
-    } else {
-        for (oi, (o, on)) in circuit.outputs.iter().enumerate() {
-            if *o < input_len_t {
-                let ou = usize::try_from(*o).unwrap();
-                if !used_inputs[ou] {
-                    writer.gen_load(usize::try_from(var_allocs[ou]).unwrap(), ou);
-                    used_inputs[ou] = true;
-                }
-                writer.gen_store(
-                    *on,
-                    oi,
-                    usize::try_from(var_allocs[usize::try_from(*o).unwrap()]).unwrap(),
-                );
-            }
-        }
+    for (oi, (o, on)) in circuit.outputs.iter().enumerate() {
+        writer.gen_store(
+            *on,
+            oi,
+            usize::try_from(var_allocs[usize::try_from(*o).unwrap()]).unwrap(),
+        );
     }
 }
 
@@ -515,8 +366,7 @@ pub fn generate_code_ext<'a, FW: FuncWriter, CW: CodeWriter<'a, FW>, T>(
     assert_eq!(basic_ops, (supported_ops & basic_ops));
     let impl_op = (supported_ops & (1u64 << InstrOp::Impl.int_value())) != 0;
     let nimpl_op = (supported_ops & (1u64 << InstrOp::Nimpl.int_value())) != 0;
-    let (var_allocs, var_num) =
-        gen_var_allocs(&circuit, &mut gen_var_usage(&circuit), single_buffer);
+    let (var_allocs, var_num) = gen_var_allocs(&circuit, &mut gen_var_usage(&circuit));
 
     let input_len = usize::try_from(circuit.input_len()).unwrap();
     let mut func_writer = writer.func_writer_ext(
@@ -531,10 +381,8 @@ pub fn generate_code_ext<'a, FW: FuncWriter, CW: CodeWriter<'a, FW>, T>(
     func_writer.func_start();
     func_writer.alloc_vars(var_num);
 
-    if single_buffer {
-        for i in 0..input_len {
-            func_writer.gen_load(usize::try_from(var_allocs[i]).unwrap(), i);
-        }
+    for i in 0..input_len {
+        func_writer.gen_load(usize::try_from(var_allocs[i]).unwrap(), i);
     }
 
     if impl_op || nimpl_op {
@@ -547,13 +395,7 @@ pub fn generate_code_ext<'a, FW: FuncWriter, CW: CodeWriter<'a, FW>, T>(
             .enumerate()
             .map(|(i, g)| g.i0 != vcircuit.gates[i].i0)
             .collect::<Vec<_>>();
-        gen_func_code_for_ximpl(
-            &mut func_writer,
-            &vcircuit,
-            &swap_args,
-            &var_allocs,
-            single_buffer,
-        );
+        gen_func_code_for_ximpl(&mut func_writer, &vcircuit, &swap_args, &var_allocs);
     } else {
         let mut vcircuit = VBinOpCircuit::from(circuit.clone());
         if optimize_negs {
@@ -567,13 +409,7 @@ pub fn generate_code_ext<'a, FW: FuncWriter, CW: CodeWriter<'a, FW>, T>(
             .enumerate()
             .map(|(i, g)| g.i0 != vcircuit.gates[i].0.i0)
             .collect::<Vec<_>>();
-        gen_func_code_for_binop(
-            &mut func_writer,
-            &vcircuit,
-            &swap_args,
-            &var_allocs,
-            single_buffer,
-        );
+        gen_func_code_for_binop(&mut func_writer, &vcircuit, &swap_args, &var_allocs);
     }
 
     func_writer.func_end();
@@ -628,13 +464,7 @@ mod tests {
         assert_eq!(vec![2, 2, 2, 2, 1, 1, 1, 1], var_usage);
         assert_eq!(
             (vec![0, 1, 2, 3, 4, 2, 0, 0], 5),
-            gen_var_allocs(&circuit, &mut var_usage, true)
-        );
-        let mut var_usage = gen_var_usage(&circuit);
-        assert_eq!(vec![2, 2, 2, 2, 1, 1, 1, 1], var_usage);
-        assert_eq!(
-            (vec![0, 1, 3, 2, 4, 2, 0, 0], 5),
-            gen_var_allocs(&circuit, &mut var_usage, false)
+            gen_var_allocs(&circuit, &mut var_usage)
         );
 
         let circuit = Circuit::new(
@@ -658,13 +488,7 @@ mod tests {
         assert_eq!(vec![2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1], var_usage);
         assert_eq!(
             (vec![0, 1, 2, 3, 4, 2, 0, 1, 5, 0, 2, 0], 6),
-            gen_var_allocs(&circuit, &mut var_usage, true)
-        );
-        let mut var_usage = gen_var_usage(&circuit);
-        assert_eq!(vec![2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1], var_usage);
-        assert_eq!(
-            (vec![0, 2, 1, 3, 2, 1, 0, 2, 4, 0, 1, 0], 5),
-            gen_var_allocs(&circuit, &mut var_usage, false)
+            gen_var_allocs(&circuit, &mut var_usage)
         );
     }
 }
