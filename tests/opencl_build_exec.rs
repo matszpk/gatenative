e2380c282d85519c4d7ecc91c5d418c9abc1ab4c
matszpk <matszpk@interia.pl>
2024-02-23 15:39:53 +0100

    Add testcase for DataTransforms for OpenCLExecutor.

--- tests/opencl_build_exec.rs
+++ tests/opencl_build_exec.rs
@@ -1053,3 +1053,54 @@ fn test_opencl_input_output_data_transformer() {
         }
     }
 }
+
+#[test]
+fn test_opencl_data_transforms() {
+    let device = Device::new(
+        *get_all_devices(CL_DEVICE_TYPE_GPU)
+            .unwrap()
+            .get(0)
+            .expect("No device in platform"),
+    );
+    let mut builder = OpenCLBuilder::new(&device, None);
+    let circuit = Circuit::new(
+        40,
+        (0..20).map(|i| Gate::new_xor(i, 20 + i)),
+        (0..20).map(|i| (40 + i, false)),
+    )
+    .unwrap();
+    builder.add("xor", circuit.clone(), None, None, None);
+    let mut execs = builder.build().unwrap();
+    let input = execs[0].new_data_from_vec(
+        (0..1024u32 * 128u32)
+            .map(|i| 124 * i + 21 * i * i + 1342)
+            .collect::<Vec<_>>(),
+    );
+    let mut it = execs[0]
+        .input_tx(64, &(0..20).chain(32..32 + 20).collect::<Vec<_>>())
+        .unwrap();
+    let mut ot = execs[0]
+        .output_tx(32, &(0..20).collect::<Vec<_>>())
+        .unwrap();
+    let mut input_circ =
+        execs[0].new_data((input.len() * it.output_elem_len()) / it.input_elem_len());
+    it.transform(&input, &mut input_circ).unwrap();
+    let output_circ = execs[0].execute(&input_circ, 0).unwrap();
+    let mut output =
+        execs[0].new_data((output_circ.len() * ot.output_elem_len()) / ot.input_elem_len());
+    ot.transform(&output_circ, &mut output).unwrap();
+    {
+        let input = input.get();
+        let input = input.get();
+        let output = output.get();
+        let output = output.get();
+        for (i, v) in output.iter().enumerate() {
+            assert_eq!(
+                (input[2 * i] ^ input[2 * i + 1]) & ((1 << 20) - 1),
+                *v,
+                "{}",
+                i
+            );
+        }
+    }
+}
