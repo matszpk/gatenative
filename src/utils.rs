8a97c777901b17f14e2495989b146ab7c20abe1c
matszpk <matszpk@interia.pl>
2024-03-07 11:01:44 +0100

    Add new tool: MultiVarAllocTool.

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,13 @@
 use gatesim::*;
 
+use std::collections::VecDeque;
 use std::fmt::Debug;
 
 use static_init::dynamic;
 use std::collections::BinaryHeap;
 use std::time::{SystemTime, UNIX_EPOCH};
 
+#[derive(Clone)]
 pub struct VarAllocator<T> {
     free_list: BinaryHeap<std::cmp::Reverse<T>>,
     alloc_map: Vec<bool>,
@@ -99,12 +101,81 @@ pub(crate) fn get_timestamp() -> u128 {
     old
 }
 
-pub(crate) const fn calc_log_bits(n: usize) -> usize {
+pub const fn calc_log_bits(n: usize) -> usize {
     let nbits = usize::BITS - n.leading_zeros();
     if (1 << (nbits - 1)) == n {
         (nbits - 1) as usize
     } else {
         nbits as usize
+    }
+}
+
+pub struct MultiVarAllocTool<T> {
+    var_allocs: Vec<VarAllocator<T>>,
+    var_usages: Vec<Vec<T>>,
+    var_new_history: Vec<VecDeque<T>>,
+    usage_mode: bool, // if true then use variables, if false then allocates variables
+}
+
+impl<T> MultiVarAllocTool<T>
+where
+    T: Default + Clone + Copy + Ord + PartialEq + Eq,
+    T: TryFrom<usize>,
+    <T as TryFrom<usize>>::Error: Debug,
+    usize: TryFrom<T>,
+    <usize as TryFrom<T>>::Error: Debug,
+{
+    pub fn new(var_type_num: usize) -> Self {
+        Self {
+            var_allocs: vec![VarAllocator::new(); var_type_num],
+            var_usages: vec![Vec::new(); var_type_num],
+            var_new_history: vec![VecDeque::new(); var_type_num],
+            usage_mode: false,
+        }
+    }
+
+    pub fn var_type_num(&self) -> usize {
+        self.var_allocs.len()
+    }
+
+    fn use_or_remove_var(&mut self, var_type: usize, v: T) {
+        let vt = usize::try_from(v).unwrap();
+        let var_usage = self.var_usages[var_type][vt];
+        let mut var_usage = usize::try_from(var_usage).unwrap();
+        assert_ne!(var_usage, 0);
+        var_usage -= 1;
+        self.var_usages[var_type][vt] = T::try_from(var_usage).unwrap();
+        if var_usage == 0 {
+            self.var_allocs[var_type].free(v);
+        }
+    }
+
+    pub fn new_var(&mut self, var_type: usize) -> T {
+        if !self.usage_mode {
+            let v = self.var_allocs[var_type].alloc();
+            let vt = usize::try_from(v).unwrap();
+            if vt >= self.var_usages[var_type].len() {
+                self.var_usages[var_type].resize(vt + 1, T::default());
+            }
+            self.var_usages[var_type][vt] = T::try_from(1).unwrap();
+            self.var_new_history[var_type].push_back(v);
+            v
+        } else {
+            let v = self.var_new_history[var_type].pop_front().unwrap();
+            self.use_or_remove_var(var_type, v);
+            v
+        }
+    }
+
+    pub fn use_var(&mut self, var_type: usize, v: T) {
+        if !self.usage_mode {
+            let vt = usize::try_from(v).unwrap();
+            let var_usage = self.var_usages[var_type][vt];
+            let var_usage = usize::try_from(var_usage).unwrap();
+            self.var_usages[var_type][vt] = T::try_from(var_usage + 1).unwrap();
+        } else {
+            self.use_or_remove_var(var_type, v);
+        }
     }
 }
 
