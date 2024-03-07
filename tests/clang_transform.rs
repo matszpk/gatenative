be1468c8d6a5b79bebdb15cd1b91d66582b0c938
matszpk <matszpk@interia.pl>
2024-03-07 17:07:42 +0100

    Update.

--- tests/clang_transform.rs
+++ tests/clang_transform.rs
@@ -5,6 +5,7 @@ fn test_clang_transform_gen_in_transform() {
     let mut transform = CLANG_TRANSFORM_U32.transform();
     transform.gen_input_transform(32);
     transform.gen_input_transform(29);
+    transform.gen_input_transform(25);
     //transform.gen_input_transform(16);
     println!("Code: {}", transform.out());
 }
