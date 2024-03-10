7a1f7fa88b3ed79c14458a64136a7f5097132152
matszpk <matszpk@interia.pl>
2024-03-10 16:26:26 +0100

    Write version of gen_output_transform that can generate routine for 1 to 32 bits.

--- tests/clang_transform_out.rs
+++ tests/clang_transform_out.rs
@@ -3,7 +3,7 @@ use gatenative::clang_transform::*;
 #[test]
 fn test_clang_transform_gen_output_transform() {
     let mut transform = CLANG_TRANSFORM_U32.transform();
-    for i in (17..=32).rev() {
+    for i in (1..=32).rev() {
         transform.gen_output_transform(i);
     }
     println!("{}", transform.out());
