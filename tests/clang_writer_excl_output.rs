bc420f3a2ae97216f1265a9c079bed77a2b710bb
matszpk <matszpk@interia.pl>
2024-03-17 19:12:38 +0100

    Next testcase.

--- tests/clang_writer_excl_output.rs
+++ tests/clang_writer_excl_output.rs
@@ -522,4 +522,83 @@ fn test_clang_writer_exclude_output() {
 }
 "##
     );
+
+    let circuit = Circuit::new(
+        3,
+        [
+            Gate::new_xor(0, 1),
+            Gate::new_xor(2, 3),
+            Gate::new_and(2, 3),
+            Gate::new_and(0, 1),
+            Gate::new_nor(5, 6),
+        ],
+        [(4, false), (5, false), (6, true), (7, true), (3, false)],
+    )
+    .unwrap();
+    let mut writer = CLANG_WRITER_U32.writer();
+    generate_code_with_config(
+        &mut writer,
+        "testcirc",
+        circuit.clone(),
+        false,
+        CodeConfig::new()
+            .exclude_outputs(Some(&[1, 3]))
+            .single_buffer(true),
+    );
+    assert_eq!(
+        &String::from_utf8(writer.out()).unwrap(),
+        r##"void gate_sys_testcirc(uint32_t* output, size_t idx) {
+    uint32_t v0;
+    uint32_t v1;
+    uint32_t v2;
+    uint32_t v3;
+    uint32_t v4;
+    v0 = output[0];
+    v1 = output[1];
+    v2 = (v0 ^ v1);
+    v3 = output[2];
+    output[2] = v2;
+    v4 = (v3 ^ v2);
+    output[0] = v4;
+    v2 = (v3 & v2);
+    v0 = (v0 & v1);
+    output[1] = ~v0;
+    v0 = ~(v2 | v0);
+}
+"##
+    );
+    let mut writer = CLANG_WRITER_INTEL_MMX.writer();
+    generate_code_with_config(
+        &mut writer,
+        "testcirc",
+        circuit.clone(),
+        false,
+        CodeConfig::new()
+            .exclude_outputs(Some(&[1, 3]))
+            .single_buffer(true),
+    );
+    assert_eq!(
+        &String::from_utf8(writer.out()).unwrap(),
+        r##"void gate_sys_testcirc(__m64* output, size_t idx) {
+    const __m64 one = *((const __m64*)one_value);
+    __m64 v0;
+    __m64 v1;
+    __m64 v2;
+    __m64 v3;
+    __m64 v4;
+    v0 = output[0];
+    v1 = output[1];
+    v2 = _m_pxor(v0, v1);
+    v3 = output[2];
+    output[2] = v2;
+    v4 = _m_pxor(v3, v2);
+    output[0] = v4;
+    v2 = _m_pand(v3, v2);
+    v0 = _m_pand(v0, v1);
+    output[1] = _m_pxor(v0, one);
+    v0 = _m_por(v2, v0);
+    _m_empty();
+}
+"##
+    );
 }
