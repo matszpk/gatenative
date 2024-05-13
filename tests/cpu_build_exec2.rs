1e13bd2a29db94f210c993d477bc6a6e3de86afd
matszpk <matszpk@interia.pl>
2024-05-13 11:20:19 +0200

    Add new cpu_build_exec testsuite.

---
+++ tests/cpu_build_exec2.rs
@@ -0,0 +1,159 @@
+use gatenative::clang_writer::*;
+use gatenative::cpu_build_exec::*;
+use gatenative::*;
+use gatesim::*;
+use gateutil::*;
+
+use std::collections::HashMap;
+use std::str::FromStr;
+
+const ADDIMMU16_77_CIRCUIT: &str = r##"    {
+        0:0n
+        1
+        2
+        3
+        4
+        5
+        6
+        7
+        8
+        9
+        10
+        11
+        12
+        13
+        14
+        15
+        xor(1,0):1
+        and(1,0)
+        xor(17,2):2n
+        and(17,2)
+        nor(18,19)
+        xor(20,3):3
+        nimpl(3,20)
+        nimpl(21,22)
+        xor(23,4):4n
+        nimpl(4,23)
+        xor(25,5):5
+        and(25,5)
+        xor(27,6):6n
+        and(27,6)
+        nor(28,29)
+        xor(30,7):7n
+        nimpl(7,30)
+        xor(32,8):8
+        and(32,8)
+        xor(34,9):9
+        and(34,9)
+        xor(36,10):10
+        and(36,10)
+        xor(38,11):11
+        and(38,11)
+        xor(40,12):12
+        and(40,12)
+        xor(42,13):13
+        and(42,13)
+        xor(44,14):14
+        and(44,14)
+        xor(46,15):15
+    }(16)
+"##;
+
+fn get_builder_configs() -> Vec<(
+    CPUExtension,
+    &'static CLangWriterConfig<'static>,
+    Option<CPUBuilderConfig>,
+)> {
+    use CPUExtension::*;
+    let no_opt_neg_config = CPUBuilderConfig {
+        optimize_negs: false,
+        parallel: None,
+    };
+    let opt_neg_config = CPUBuilderConfig {
+        optimize_negs: true,
+        parallel: None,
+    };
+
+    let mut configs = vec![
+        (NoExtension, &CLANG_WRITER_U64_TEST_IMPL, None),
+        (NoExtension, &CLANG_WRITER_U64_TEST_NIMPL, None),
+        (NoExtension, &CLANG_WRITER_U64, Some(no_opt_neg_config)),
+        (NoExtension, &CLANG_WRITER_U64, Some(opt_neg_config)),
+    ];
+    #[cfg(target_pointer_width = "32")]
+    configs.push((NoExtension, &CLANG_WRITER_U32, None));
+    #[cfg(target_pointer_width = "64")]
+    configs.push((NoExtension, &CLANG_WRITER_U64, None));
+
+    if *CPU_EXTENSION == IntelAVX512
+        || *CPU_EXTENSION == IntelAVX2
+        || *CPU_EXTENSION == IntelAVX
+        || *CPU_EXTENSION == IntelSSE2
+        || *CPU_EXTENSION == IntelSSE
+        || *CPU_EXTENSION == IntelMMX
+    {
+        configs.push((IntelMMX, &CLANG_WRITER_INTEL_MMX, None));
+    }
+    if *CPU_EXTENSION == IntelAVX512
+        || *CPU_EXTENSION == IntelAVX2
+        || *CPU_EXTENSION == IntelAVX
+        || *CPU_EXTENSION == IntelSSE2
+        || *CPU_EXTENSION == IntelSSE
+    {
+        configs.push((IntelSSE, &CLANG_WRITER_INTEL_SSE, None));
+    }
+    if *CPU_EXTENSION == IntelAVX512
+        || *CPU_EXTENSION == IntelAVX2
+        || *CPU_EXTENSION == IntelAVX
+        || *CPU_EXTENSION == IntelSSE2
+    {
+        configs.push((IntelSSE2, &CLANG_WRITER_INTEL_SSE2, None));
+    }
+    if *CPU_EXTENSION == IntelAVX512 || *CPU_EXTENSION == IntelAVX2 || *CPU_EXTENSION == IntelAVX {
+        configs.push((IntelAVX, &CLANG_WRITER_INTEL_AVX, None));
+    }
+    if *CPU_EXTENSION == IntelAVX512 || *CPU_EXTENSION == IntelAVX2 {
+        configs.push((IntelAVX2, &CLANG_WRITER_INTEL_AVX2, None));
+    }
+    if *CPU_EXTENSION == IntelAVX512 {
+        configs.push((IntelAVX512, &CLANG_WRITER_INTEL_AVX512, None));
+    }
+    if *CPU_EXTENSION == ARMNEON {
+        configs.push((ARMNEON, &CLANG_WRITER_ARM_NEON, None));
+    }
+    // double for parallel
+    let configs = configs
+        .clone()
+        .into_iter()
+        .chain(
+            configs
+                .clone()
+                .into_iter()
+                .map(|(ext, clang_config, cpu_builder_config)| {
+                    let mut cpu_builder_config =
+                        cpu_builder_config.unwrap_or(CPU_BUILDER_CONFIG_DEFAULT);
+                    cpu_builder_config.parallel = Some(64);
+                    (ext, clang_config, Some(cpu_builder_config))
+                }),
+        )
+        .collect::<Vec<_>>();
+    configs
+}
+
+#[test]
+fn test_cpu_builder_and_exec_inner_loop() {
+    let configs = get_builder_configs();
+    let mut mul_add_data_map = HashMap::<usize, (CPUDataHolder, Vec<u32>)>::new();
+    let addu16_circuit = Circuit::<u32>::from_str(ADDIMMU16_77_CIRCUIT).unwrap();
+    for (config_num, (cpu_ext, writer_config, builder_config)) in configs.into_iter().enumerate() {
+        const ITER_NUM: u32 = 12;
+        let mut builder =
+            CPUBuilder::new_with_cpu_ext_and_clang_config(cpu_ext, writer_config, builder_config);
+        builder.add_with_config(
+            "addu16",
+            addu16_circuit.clone(),
+            CodeConfig::new().inner_loop(Some(ITER_NUM)),
+        );
+        let mut execs = builder.build().unwrap();
+    }
+}
