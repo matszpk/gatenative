18eb76770cc6dcfd8769b2f4809d7d7fd02b3481
matszpk <matszpk@interia.pl>
2024-05-20 08:43:23 +0200

    Write gen_rev_subtree_dependencies.

--- src/vlop3circuit.rs
+++ src/vlop3circuit.rs
@@ -264,6 +264,29 @@ where
     coverage
 }
 
+// argument - return from subtree_dependencies
+// return: index - subtree index, value - list of subtree's children (subtree index)
+fn gen_rev_subtree_dependencies<T>(deps: Vec<Vec<(T, T, bool)>>) -> Vec<Vec<T>>
+where
+    T: Clone + Copy + Ord + PartialEq + Eq,
+    T: Default + TryFrom<usize>,
+    <T as TryFrom<usize>>::Error: Debug,
+    usize: TryFrom<T>,
+    <usize as TryFrom<T>>::Error: Debug,
+{
+    let mut revdeps = vec![vec![]; deps.len()];
+    for (i, deplist) in deps.into_iter().enumerate() {
+        for (ni, _, _) in deplist {
+            revdeps[usize::try_from(ni).unwrap()].push(T::try_from(i).unwrap());
+        }
+    }
+    for dep in &mut revdeps {
+        dep.sort();
+        dep.dedup();
+    }
+    revdeps
+}
+
 impl<T> From<Circuit<T>> for VLOP3Circuit<T>
 where
     T: Clone + Copy + Ord + PartialEq + Eq + Hash,
