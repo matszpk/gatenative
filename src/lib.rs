56ceaa323813784cb4894e08a2d99ebaac70c966
matszpk <matszpk@interia.pl>
2024-06-26 06:44:53 +0200

    Add info about using DataTransforms.

--- src/lib.rs
+++ src/lib.rs
@@ -1256,6 +1256,9 @@ where
     fn preferred_input_count(&self) -> usize;
 }
 
+// About using DataTransformers and DataTransforms.
+// This data transformations designed to be light and used only once,
+// because they are not too much fast.
 pub trait DataTransformer<'a, DR: DataReader, DW: DataWriter, D: DataHolder<'a, DR, DW>> {
     type ErrorType;
 
